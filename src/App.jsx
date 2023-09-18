b2cc7c46c96cdcbbb519446152d7dc73cb4a1c49
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-09-18 21:09:43 +0800

    Create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import "./scss/main.scss";
+import Navbar from "./layout/Navbar";
+
 function App() {
   return (
     <>
-      <p>Hello</p>
+      <Navbar />
     </>
   );
 }
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,28 @@
+function Navbar() {
+  return (
+    <nav>
+      <div className="navbar-wrapper">
+        <a href="/">Logo</a>
+        <ul>
+          <li>
+            <a href="/"> About</a>
+          </li>
+          <li>
+            <a href="/"> Tech Stack</a>
+          </li>
+          <li>
+            <a href="/"> Experience</a>
+          </li>
+          <li>
+            <a href="/"> Portfolio</a>
+          </li>
+          <li>
+            <a href="/"> Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
