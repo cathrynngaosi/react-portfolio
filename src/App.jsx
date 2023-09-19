50b9f683c4611d9f8d5d444b39ce5766e4bef337
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-09-19 10:29:22 +0800

    Revert "Create intro section"
    This reverts commit 4ae5489ca6e0a955b40eddfcd83288225d2ffb26.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./scss/main.scss";
 import Navbar from "./layout/Navbar";
-import Intro from "./sections/Intro";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Intro />
     </>
   );
 }
