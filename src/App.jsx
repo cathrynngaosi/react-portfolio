74b5345cc64dfa68543edc6c08607a4c5c84c36d
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-09-19 11:05:16 +0800

    Create About me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./scss/main.scss";
 import Navbar from "./layout/Navbar";
 import Intro from "./sections/Intro";
+import About from "./sections/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Intro />
+      <About />
     </>
   );
 }
---
+++ src/sections/About.jsx
@@ -0,0 +1,26 @@
+function About() {
+  return (
+    <section id="about">
+      <div className="about-container">
+        <div className="about-photo">Picture</div>
+        <div className="about-text">
+          <h1>ABOUT ME</h1>
+          <p>
+            Posuere morbi leo urna molestie at elementum eu facilisis. Nulla
+            porttitor massa id neque aliquam vestibulum. Cras adipiscing enim eu
+            turpis egestas. Hac habitasse platea dictumst quisque sagittis
+            purus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+            autem, quam numquam tempore deserunt temporibus quas porro ut
+            ducimus quae, repellendus accusantium atque! Saepe non, possimus
+            nulla vel quibusdam repellendus.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
